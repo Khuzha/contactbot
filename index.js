e7b464be7847b3e553979ca1f822b1d2125829db
Khuzha <t.sardorkhuja@gmail.com>
2018-09-13 20:41:37 +0500

    Added working code

---
+++ index.js
@@ -0,0 +1,29 @@
+const telegraf = require('telegraf')
+const data = require('./data')
+const bot = new telegraf(data.token)
+
+bot.start((ctx) => {
+  if (ctx.chat.id == data.myid){
+    ctx.reply(data.startMessageToMe)
+  } else {
+  ctx.reply(data.hello/*, {reply_markup: {inline_keyboard: [[{text: '🇬🇧 Change lang', callback_data: 'en'}]]}}*/)
+  }
+})
+
+bot.on('text', (ctx) => {
+  if (ctx.chat.id == data.myid){
+    if(ctx.update.message && ctx.update.message.reply_to_message && ctx.message.reply_to_message.forward_from) {
+
+      if(!ctx.message.reply_to_message.forward_from.is_bot){
+                  data.lastID = ctx.message.reply_to_message.forward_from.id;
+                  ctx.telegram.sendMessage(data.lastID, ctx.message.text);
+              }
+          } else {
+      ctx.reply(data.emptyReciever)
+    }
+  } else {
+    ctx.forwardMessage(data.myid, ctx.from.id, ctx.message.id)
+  }
+})
+
+bot.startPolling()
