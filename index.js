265422c90bc39f5b2416d7cd2c66c072d2545769
Khuzha <mr.khuzha@gmail.com>
2018-11-05 14:49:14 +0500

    ..

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const telegraf = require('telegraf')
-const data = require('./data')
-const bot = new telegraf(data.token)
+const config = require('./config')
+const bot = new telegraf(config.token)
 
 bot.start((ctx) => {
-  if (ctx.chat.id == data.myid){
-    ctx.reply(data.startMessageToMe)
+  if (ctx.chat.id == config.myid){t
+    ctx.reply(config.startMessageToMe)
   } else {
-  ctx.reply(data.hello/*, {reply_markup: {inline_keyboard: [[{text: '🇬🇧 Change lang', callback_data: 'en'}]]}}*/)
+  ctx.reply(config.hello/*, {reply_markup: {inline_keyboard: [[{text: '🇬🇧 Change lang', callback_config: 'en'}]]}}*/)
   }
 })
 
 bot.on('text', (ctx) => {
-  if (ctx.chat.id == data.myid){
+  if (ctx.chat.id == config.myid){
     if(ctx.message) {
     if (ctx.message.reply_to_message && ctx.message.reply_to_message.forward_from) {
         if(!ctx.message.reply_to_message.forward_from.is_bot){
-            data.lastID = ctx.message.reply_to_message.forward_from.id;
-            ctx.telegram.sendMessage(data.lastID, ctx.message.text);
+            config.lastID = ctx.message.reply_to_message.forward_from.id;
+            ctx.telegram.sendMessage(config.lastID, ctx.message.text);
         }
     } else {
-        if (data.lastID) ctx.telegram.sendMessage(data.lastID, ctx.message.text);
+        if (config.lastID) ctx.telegram.sendMessage(config.lastID, ctx.message.text);
     }
 } else {
-  ctx.reply(data.emptyReciever)
+  ctx.reply(config.emptyReciever)
 }
 } else {
-    ctx.forwardMessage(data.myid, ctx.from.id, ctx.message.id)
+    ctx.forwardMessage(config.myid, ctx.from.id, ctx.message.id)
   }
 })
 
