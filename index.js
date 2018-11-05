6132caf380033608fbeff4f0ed84e9d64664fe7e
Khuzha <mr.khuzha@gmail.com>
2018-11-05 16:31:22 +0500

    Update 2.0 - beta

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const telegraf = require('telegraf')
 const config = require('./config')
 const token = require('./token')
-const bot = new telegraf(token)
-
+const bot = new telegraf(token.tok)
+let lastId
 
 bot.start((ctx) => {
-  if (ctx.chat.id == config.myid){t
+  if (ctx.chat.id == config.myid){
     ctx.reply(config.startMessageToMe)
   } else {
   ctx.reply(config.hello/*, {reply_markup: {inline_keyboard: [[{text: '🇬🇧 Change lang', callback_config: 'en'}]]}}*/)
@@ -13,26 +13,64 @@ bot.start((ctx) => {
 })
 
 bot.on('sticker', (ctx) => {
-  console.log(ctx.message)
-})
-
-bot.on('text', (ctx) => {
-  if (ctx.chat.id == config.myid){
-    if(ctx.message) {
-    if (ctx.message.reply_to_message && ctx.message.reply_to_message.forward_from) {
-        if(!ctx.message.reply_to_message.forward_from.is_bot){
-            config.lastID = ctx.message.reply_to_message.forward_from.id;
-            ctx.telegram.sendMessage(config.lastID, ctx.message.text);
-        }
+  if (config.myid == ctx.chat.id) {
+    if ('reply_to_message' in ctx.message) {
+      bot.telegram.sendSticker(ctx.message.reply_to_message.forward_from.id, ctx.message.sticker.file_id)
+        .catch((err) => sendError(err, ctx))
+      lastId = ctx.message.reply_to_message.forward_from.id
     } else {
-        if (config.lastID) ctx.telegram.sendMessage(config.lastID, ctx.message.text);
+      if (lastId != undefined) {
+        bot.telegram.sendSticker(lastId, ctx.message.sticker.file_id)
+          .catch((err) => sendError(err, ctx))
+      } else {
+        ctx.reply('Выбери, кому отвечать.')
+      }
     }
-} else {
-  ctx.reply(config.emptyReciever)
-}
-} else {
+  } else {
     ctx.forwardMessage(config.myid, ctx.from.id, ctx.message.id)
   }
 })
+
+bot.on('text', (ctx) => {
+  if (config.myid == ctx.chat.id) {
+    if ('reply_to_message' in ctx.message) {
+      bot.telegram.sendMessage(ctx.message.reply_to_message.forward_from.id, ctx.message.text)
+        .catch((err) => sendError(err, ctx))
+      lastId = ctx.message.reply_to_message.forward_from.id
+    } else {
+      console.log(lastId)
+      if (lastId != undefined) {
+        bot.telegram.sendMessage(lastId, ctx.message.text)
+          .catch((err) => sendError(err, ctx))
+      } else {
+        ctx.reply('Выбери, кому отвечать.')
+      }
+    }
+  } else {
+    ctx.forwardMessage(config.myid, ctx.from.id, ctx.message.id)
+  }
+})
+
+function sendError (err, ctx) {
+  console.log(err)
+  if (err.response.description != 'Forbidden: bot was blocked by the user') {
+    if (err != undefined && err != null)
+      if (ctx != undefined) {
+        bot.telegram.sendMessage(62253745, 'Ошибка у [' + ctx.chat.first_name + '](tg://user?id=' + ctx.chat.id + ')\nТекст ошибки:\n ' + err.toString(), {parse_mode: 'markdown'})
+          .catch((err) => console.log(err))
+      } else {
+        bot.telegram.sendMessage(62253745, 'Ошибка!\nТекст ошибки:\n ' + JSON.stringify(err), {parse_mode: 'markdown'})
+          .catch((err) => console.log(err))
+      }
+  } else {
+    bot.telegram.sendMessage(config.myid, 'Пользователь заблокировал бота.')
+  }
+
+}
+
+bot.catch((err) => {
+  console.log(err)
+})
+
 
 bot.startPolling()
